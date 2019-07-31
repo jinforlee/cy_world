e362a5176926ee09618542df9c5a4f701b67b3b4
jinforlee <genny961213@gmail.com>
2019-07-31 06:37:14 +0000

    cy_fir

---
+++ home/.bash_history
@@ -0,0 +1,5 @@
+python manage.py runserver $IP:$PORT
+ls
+cd cy/
+python manage.py runserver $IP:$PORT
+pipenv shell
---
+++ home/.local/share/virtualenvs/workspace-jUz2GyNd/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/ubuntu/.local/share/virtualenvs/workspace-jUz2GyNd/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
